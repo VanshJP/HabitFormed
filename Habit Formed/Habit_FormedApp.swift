5fa3d8d81435889956706e64156e701bb3445d13
Vansh <patelvansh1023@gmail.com>
2026-04-27 14:53:51 -0400

    Initial Commit

---
+++ Habit Formed/Habit_FormedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_FormedApp.swift
+//  Habit Formed
+//
+//  Created by Vansh Patel on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Habit_FormedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
